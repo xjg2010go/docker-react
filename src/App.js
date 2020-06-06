4177bfe8272444bb9bae3f88d1e0ccc566feb4de
lxin <lxin@appannie.com>
2020-06-06 17:40:18 +0800

    add travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          bye nihao <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          bye there xxx!
+          Learn React
         </a>
       </header>
     </div>
